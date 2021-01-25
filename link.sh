bfb7290c72cc6140750567b05ca0770780d23178
Max Justus Spransy <maxjustus@gmail.com>
2021-01-25 15:12:02 -0800

    Update

--- link.sh
+++ link.sh
@@ -6,3 +6,6 @@ ln -nfs `pwd`/.tmux.conf.local ~/
 ln -nfs `pwd`/.vimrc.local ~/
 ln -nfs `pwd`/.vimbundle.local ~/
 ln -nfs `pwd`/merge_and_tag /usr/local/bin
+mkdir -p ~/.config/nvim/autoload
+ln -nfs ~/.vimrc ~/.config/nvim/init.vim
+ln -nfs ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
